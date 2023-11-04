3b389ae21e85d38989509b8e290525c6ac54db6c
amazake13 <souamano@icloud.com>
2023-11-04 15:14:16 +0900

    Initial Commit

---
+++ CoreMotionApp/CoreMotionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreMotionAppApp.swift
+//  CoreMotionApp
+//
+//  Created by 天野創 on 2023/11/04.
+//
+
+import SwiftUI
+
+@main
+struct CoreMotionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
